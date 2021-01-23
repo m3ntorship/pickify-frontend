ce769e1f9e1db69e5f77f3944aefdd692051e991
Mohamed <mahameds19999@gmail.com>
2021-01-23 15:01:50 +0200

    Add redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { PropTypes } from 'prop-types';
+
+import withRedux from 'next-redux-wrapper';
+import { Provider } from 'react-redux';
+import initStore from '../store';
+
+function Pickly({ Component, pageProps, store }) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+Pickly.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    .isRequired,
+  store: PropTypes.object.isRequired,
+};
+
+export default withRedux(initStore)(Pickly);
