e9cbf113e33e4d7997ef0b122d22eea419310bd6
AhmedAdel999 <ahmedadel555441@gmail.com>
2021-05-10 16:00:48 +0200

    created slider component

---
+++ modules/shared/components/atoms/Slider/Slider.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Slider from '.';
+
+describe('snapshot testing', () => {
+  it('should render horizontally when given horizontal property', () => {
+    const tree = renderer
+      .create(<Slider type="horizontal" progress={50} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render vertically when given vertical property', () => {
+    const tree = renderer
+      .create(
+        <Slider
+          type="vertical"
+          progress={50}
+          height={300}
+          verticalMeterColor="primary"
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render circular when given circular property', () => {
+    const tree = renderer
+      .create(<Slider type="cicular" progress={50} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
