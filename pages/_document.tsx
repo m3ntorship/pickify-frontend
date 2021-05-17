7a2c914200be1610ef6ff8dba943094c36c15f1b
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2021-05-17 15:39:44 +0200

    Remove getInitialProps in the document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,6 @@
-import type { DocumentContext, DocumentInitialProps } from 'next/document';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  public static async getInitialProps<T extends DocumentContext>(
-    ctx: T,
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
-    return initialProps;
-  }
-
   public render(): JSX.Element {
     return (
       <Html>
