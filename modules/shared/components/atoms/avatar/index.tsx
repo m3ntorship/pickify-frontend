c5a9a1f49eb3caf308ba301265721337df138a3a
AhmedAdel999 <ahmedadel555441@gmail.com>
2021-05-18 23:47:05 +0200

    maintained fixed aspcet ration for any image

--- modules/shared/components/atoms/avatar/index.tsx
+++ modules/shared/components/atoms/avatar/index.tsx
@@ -1,9 +1,10 @@
 import type { FC, ReactElement } from 'react';
 import React from 'react';
 import Image from 'next/image';
+import classNames from 'classnames';
 import type { IAvatar } from './IAvatar';
-import AnonymousIcon from './anonymous.svg';
-import NotFilledIcon from './notFilled.svg';
+import AnonymousIcon from '../../icons/anonymous.svg';
+import NotFilledIcon from '../../icons/notFilled.svg';
 
 export const determineAvatarSize = (componentSize: string): number => {
   const smallSize = 32;
@@ -19,13 +20,37 @@ export const determineAvatarSize = (componentSize: string): number => {
 };
 
 const Index: FC<IAvatar.IProps> = ({ size, variant, imgSrc }): ReactElement => {
+  const wrapperClasses = classNames('relative rounded-full overflow-hidden', {
+    'w-8 h-8': size === 'small',
+    'w-10 h-10': size === 'medium',
+    'w-14 h-14': size === 'large',
+  });
   const avatarSize = determineAvatarSize(size);
-  if (variant === 'anonymous') {
-    return <AnonymousIcon width={avatarSize} height={avatarSize} />;
-  }
-  if (variant === 'filled' && imgSrc !== undefined)
-    return <Image height={avatarSize} width={avatarSize} src={imgSrc} />;
-
-  return <NotFilledIcon width={avatarSize} height={avatarSize} />;
+  return (
+    <div className={wrapperClasses}>
+      {variant === 'filled' && imgSrc !== undefined && (
+        <Image
+          height={avatarSize}
+          width={avatarSize}
+          className="absolute w-full h-full"
+          src={imgSrc}
+        />
+      )}
+      {(variant === 'anonymous' || imgSrc === undefined) && (
+        <AnonymousIcon
+          className="absolute w-full h-full"
+          width={avatarSize}
+          height={avatarSize}
+        />
+      )}
+      {variant === 'notFilled' && (
+        <NotFilledIcon
+          className="absolute w-full h-full"
+          width={avatarSize}
+          height={avatarSize}
+        />
+      )}
+    </div>
+  );
 };
 export default Index;
