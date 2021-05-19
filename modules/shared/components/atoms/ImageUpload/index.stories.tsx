68e93962a96f24db84be54edd3338024c222c7b5
Ahmed Elsayad <ahmedelsayad98@gmail.com>
2021-05-19 16:29:37 +0200

    minor change

--- modules/shared/components/atoms/ImageUpload/index.stories.tsx
+++ modules/shared/components/atoms/ImageUpload/index.stories.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import type { ReactElement } from 'react';
 import type { Story } from '@storybook/react';
 import ImageUpload from '.';
-import type { IImageUpload } from './IImageUpload';
 
 export default {
   component: ImageUpload,
   title: 'Atoms/ImageUpload',
 };
 
-const Template: Story<IImageUpload.IProps> = (args): ReactElement => (
-  <ImageUpload {...args} />
-);
+const Template: Story = (args): ReactElement => <ImageUpload {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
