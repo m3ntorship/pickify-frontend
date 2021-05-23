cc0e698b85db59a3c45c0dc05fe3a7374e296ab0
Ahmed <52901348+atwfk@users.noreply.github.com>
2021-05-23 14:09:16 +0200

    fix `maximumProgress` typo mistake and added its type

--- modules/shared/logic/votesLogic/votesLogic.ts
+++ modules/shared/logic/votesLogic/votesLogic.ts
@@ -2,8 +2,8 @@ export const calcTotalVotes = (votesGroup: number[]): number =>
   votesGroup.reduce((accumulator, currentValue) => accumulator + currentValue);
 
 export const calcProgress = (votes: number, totalVotes: number): number => {
-  const maximumProfress = 100;
-  const progress: number = (votes / totalVotes) * maximumProfress;
+  const maximumProgress: number = 100;
+  const progress: number = (votes / totalVotes) * maximumProgress;
 
   return Math.round(progress);
 };
