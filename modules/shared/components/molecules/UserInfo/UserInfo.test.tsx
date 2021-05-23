0ab926393d8e45dd0e3077ef322f4f24f850d071
omarawad0 <ogmal01@gmail.com>
2021-05-23 21:26:37 +0200

    changed test() to it() and handled a typo

--- modules/shared/components/molecules/UserInfo/UserInfo.test.tsx
+++ modules/shared/components/molecules/UserInfo/UserInfo.test.tsx
@@ -3,12 +3,12 @@ import { render, screen } from '@testing-library/react';
 import UserInfo from './Index';
 
 describe('UserInfo', () => {
-  test('if variant equalls annonymous, name becomes Anonymous as well', () => {
+  it('if variant equalls annonymous, name becomes Anonymous as well', () => {
     render(<UserInfo size="medium" variant="anonymous" date="2 hours" />);
     expect(screen.getByTestId('name')).toHaveTextContent('Anonymous');
   });
 
-  test('if variant equalls annonymous, name becomes Anonymous as well even if a name is set', () => {
+  it('if variant equalls annonymous, name becomes Anonymous as well even if a name is set', () => {
     render(
       <UserInfo
         size="medium"
@@ -20,7 +20,7 @@ describe('UserInfo', () => {
     expect(screen.getByTestId('name')).toHaveTextContent('Anonymous');
   });
 
-  test('if variant equalls filled, name is whatever passed to the name prop', () => {
+  it('if variant equalls filled, name is whatever passed to the name prop', () => {
     render(
       <UserInfo
         size="medium"
@@ -32,11 +32,11 @@ describe('UserInfo', () => {
     expect(screen.getByTestId('name')).toHaveTextContent('Ahmed Ayoub');
   });
 
-  test('if variant equalls notFilled, name is whatever passed to the name prop', () => {
+  it('if variant equalls notFilled, name is whatever passed to the name prop', () => {
     render(
       <UserInfo
         size="medium"
-        variant="filled"
+        variant="notFilled"
         date="2 hours"
         name="Ahmed Ayoub"
       />,
