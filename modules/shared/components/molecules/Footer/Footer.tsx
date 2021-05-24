b01534a11c1b04bb5dce3253fdef5fb8a0952d3d
Ahmed Elsayad <ahmedelsayad98@gmail.com>
2021-05-24 02:32:57 +0200

    creating the footer component

--- modules/shared/components/molecules/Footer/Footer.tsx
+++ modules/shared/components/molecules/Footer/Footer.tsx
@@ -7,14 +7,15 @@ import styles from './Footer.module.css';
 const Footer: FC<IFooter.IProps> = ({
   numberOfVotes,
   showResult = false,
+  handleClick,
 }): ReactElement => {
   return (
     <div className={styles.container}>
       {!showResult && <span> Vote to uncover the total number of voters </span>}
       {showResult && <span>{numberOfVotes} votes</span>}
-      <span>
+      <button type="button" onClick={handleClick}>
         <Share />
-      </span>
+      </button>
     </div>
   );
 };
--- modules/shared/components/molecules/Footer/IFooter.d.ts
+++ modules/shared/components/molecules/Footer/IFooter.d.ts
@@ -2,6 +2,7 @@ declare namespace IFooter {
   export interface IProps {
     numberOfVotes?: number;
     showResult: boolean;
+    handleClick?: () => void;
   }
 }
 export { IFooter };
