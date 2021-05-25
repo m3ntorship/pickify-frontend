ad7c7b19991287cb2c84e8c50a2ea0217855c712
AhmedElshaf3i <ahmed.m.elshafei@gmail.com>
2021-05-25 23:47:51 +0200

    eddit testing  to  snapshots

--- modules/shared/components/molecules/imageCaption/imgCaption.test.tsx
+++ modules/shared/components/molecules/imageCaption/imgCaption.test.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import * as renderer from 'react-test-renderer';
 import ImageCaption from '.';
 
-describe('imageCaption', () => {
+describe('imageCaption snapshots', () => {
   it('should render caption with text (Black T-shirt) and letter (A) when given this as property', () => {
-    const { getByText } = render(
-      <ImageCaption imgCaptionLetter="A" imgCaption="Black T-shirt" />,
-    );
-    expect(getByText('Black T-shirt')).toBeInTheDocument();
-    expect(getByText('A')).toBeInTheDocument();
+    const tree = renderer
+      .create(<ImageCaption imgCaptionLetter="A" imgCaption="Black T-shirt" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
   it('should render caption without letter and divider when not given this as a property', () => {
-    const { getByTestId } = render(<ImageCaption imgCaption="Black T-shirt" />);
-    expect(getByTestId('captionLetter')).toHaveClass('hidden');
-    expect(getByTestId('captionDivider')).toHaveClass('hidden');
-    // expect(getByTestId('captionLetter')).not.toBeVisible()
-    // expect(getByTestId('captionDivider')).not.toBeVisible();
+    const tree = renderer
+      .create(<ImageCaption imgCaption="Black T-shirt" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
