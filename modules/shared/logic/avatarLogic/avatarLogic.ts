a0b771ecdc60cc9be8f8d5324a101af8ae1f6a08
AhmedAdel999 <ahmedadel555441@gmail.com>
2021-05-31 21:17:49 +0200

    Added documentation comment to avatarLogic

--- modules/shared/logic/avatarLogic/avatarLogic.ts
+++ modules/shared/logic/avatarLogic/avatarLogic.ts
@@ -1,3 +1,8 @@
+/**
+ * This function accepts a string as a parameter and return a number representing the desired size
+ * @param componentSize - The function parameter
+ * @returns number representing avatar size
+ */
 export const determineAvatarSize = (componentSize: string): number => {
   const smallSize = 32;
   const mediumSize = 40;
