99cad87bfa4ca8bbeac8e3e2d027cf6c0082bb2b
Ahmed Ayoub <great.nile@gmail.com>
2021-06-03 01:23:01 +0200

    mock fetch data

--- modules/shared/api/getPosts.api.ts
+++ modules/shared/api/getPosts.api.ts
@@ -1,21 +1,31 @@
+/* eslint-disable */
 import type { AxiosError } from 'axios';
 import type { IGetPosts } from './IGetPosts';
 import { postsApi } from './postsApi.api';
+import { mockedData } from './postsMockedData';
 
-export const getPosts = async (): Promise<IGetPosts.IData> => {
-  const notFound = 404;
-  return postsApi
-    .getPosts()
-    .then(({ data }) => {
-      return { data };
-    })
-    .catch((error: AxiosError) => {
-      return {
-        data: {
-          error: true,
-          errorCode: error.response ? error.response.status : notFound,
-          message: error.message,
-        },
-      };
-    });
-};
+// export const getPosts = async (): Promise<IGetPosts.IData> => {
+//   const notFound = 404;
+//   return postsApi
+//     .getPosts()
+//     .then(({ data }) => {
+//       return { data };
+//     })
+//     .catch((error: AxiosError) => {
+//       return {
+//         data: {
+//           error: true,
+//           errorCode: error.response ? error.response.status : notFound,
+//           message: error.message,
+//         },
+//       };
+//     });
+// };
+
+export const getPosts = async (): Promise<any> =>
+  Promise.resolve({ data: mockedData });
+// Promise.reject({
+//   error: true,
+//   errorCode: 500,
+//   message: 'sabba7',
+// });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,49 @@
+/* eslint-disable */
 import Head from 'next/head';
 import { getPosts } from '@modules/shared/api/getPosts.api';
 import type { FC, ReactElement } from 'react';
 import type { GetServerSideProps } from 'next';
 import type { IPostFeed } from '@modules/shared/types/postFeed/IPostFeed';
+import withErrorHandler from '@modules/shared/components/HOC/WithErrorHandler/WithErrorHandler';
+
+interface ITempTextPollFeed {
+  id: string;
+}
+const TempTextPoll = ({ post }: { post: any }) => {
+  return <h2>{post.id}</h2>;
+};
+const TempTextPollFeed = ({ data }: { data: any }) => {
+  return data.posts.map((post: any) => {
+    return <TempTextPoll key={post.id} post={post} />;
+  });
+};
+
+const TempTextPollFeedWithError = withErrorHandler(TempTextPollFeed);
 
 const Home: FC<IPostFeed.Posts> = ({ data }): ReactElement => {
+  console.log(data);
   return (
     <>
       <Head>
         <title>Pickly | Pick your choice</title>
       </Head>
-      <div className="w-96 m-6">
-        <div className="h-screen flex font-bold">
-          <h1>Pickly</h1>
-          <h1>{data.postsCount}</h1>
-        </div>
-      </div>
+
+      <TempTextPollFeedWithError data={data} />
     </>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const { data } = await getPosts();
-  return {
-    props: {
-      data,
-    },
-  };
+  try {
+    const { data } = await getPosts();
+    return {
+      props: { data },
+    };
+  } catch (error) {
+    return {
+      props: { data: error },
+    };
+  }
 };
 
 export default Home;
