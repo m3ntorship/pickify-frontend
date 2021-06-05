12bf3d61a70450e551b9f2ed788233a8b4c2ea2b
AhmedAdel999 <ahmedadel555441@gmail.com>
2021-06-05 22:12:04 +0200

    removed unnecessary comment and added outline-none

--- modules/shared/components/atoms/VoteIcon/VoteIcon.tsx
+++ modules/shared/components/atoms/VoteIcon/VoteIcon.tsx
@@ -11,7 +11,7 @@ const VoteIcon: FC<IVoteIcon.IProps> = ({ isOneImageVote }): ReactElement => {
     <div className="flex justify-center">
       {!isOneImageVote && (
         <button type="button" className={`${styles.love} group`}>
-          <FilledHeartIcon className="fill-primary stroke-current stroke-1 text-white group-hover:fill-white" />
+          <FilledHeartIcon className="fill-primary stroke-current stroke-2 text-white group-hover:fill-white" />
         </button>
       )}
       {isOneImageVote && (
@@ -20,7 +20,6 @@ const VoteIcon: FC<IVoteIcon.IProps> = ({ isOneImageVote }): ReactElement => {
             <FilledDislikeIcon className="fill-error stroke-current stroke-1 text-white group-hover:fill-white" />
           </button>
           <button type="button" className={`${styles.like} group`}>
-            {/* <OutlinedLikeIcon className="group-hover:hidden" /> */}
             <FilledLikeIcon className="fill-primary stroke-current stroke-1 text-white group-hover:fill-white" />
           </button>
         </div>
