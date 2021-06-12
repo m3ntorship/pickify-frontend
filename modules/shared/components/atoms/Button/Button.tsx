75d592eb171e86af1c3014d9f6347be75be38e47
ahmedelsayad11 <68955535+ahmedelsayad11@users.noreply.github.com>
2021-06-12 22:32:40 +0200

    done some adjustment to improve the button atom (#179)
    * done some adjustment to improve the atom
    
    * added needed tailwind value and adjusted the atom to be more reusable
    
    * updated the snap test
    
    * correct spelling in the enum file
    
    * added the normal size to the tertiary button
    
    * done some requsted changes in the names of the variable
    
    * adjusted the types of the button
    
    * give default value for the buttonType
    
    * updated the tests
    
    Co-authored-by: Ahmed Elsayad <ahmedelsayad98@gmail.com>

--- modules/shared/components/atoms/Button/Button.tsx
+++ modules/shared/components/atoms/Button/Button.tsx
@@ -3,8 +3,8 @@ import classnames from 'classnames';
 import type { FC, ReactElement } from 'react';
 import type { IButton } from './types/IButton';
 import styles from './Button.module.css';
-import ButtonArrowDown from '../../icons/buttonArowDown.svg';
-
+import ArrowDownIcon from '../../icons/buttonArowDown.svg';
+// THE normal prop should only be passed with the tertiary variant
 const Button: FC<IButton.IProps> = ({
   children = 'Button',
   disabled = false,
@@ -13,6 +13,7 @@ const Button: FC<IButton.IProps> = ({
   leftIcon = false,
   rightIcon = false,
   onlyIcon = false,
+  buttonType = 'button',
   onClick,
 }): ReactElement => {
   const btnClasses = classnames(
@@ -20,11 +21,14 @@ const Button: FC<IButton.IProps> = ({
     {
       [styles['btn-primary']]: variant === 'primary',
       [styles['btn-secondary']]: variant === 'secondary',
+      [styles['btn-quaternary']]: variant === 'quaternary',
       [styles['btn-text']]: variant === 'text',
     },
     {
+      [styles['btn-xlarge']]: size === 'xlarge',
       [styles['btn-large']]: size === 'large',
       [styles['btn-medium']]: size === 'medium',
+      [styles['btn-normal']]: size === 'normal',
       [styles['btn-small']]: size === 'small',
     },
     {
@@ -36,27 +40,28 @@ const Button: FC<IButton.IProps> = ({
 
   return (
     <button
-      type="button"
+      /* eslint-disable react/button-has-type */
+      type={buttonType}
       onClick={onClick}
       className={btnClasses}
       disabled={disabled}
     >
       {leftIcon && (
         <span>
-          <ButtonArrowDown />
+          <ArrowDownIcon />
         </span>
       )}
 
       {onlyIcon ? (
         <span>
-          <ButtonArrowDown />
+          <ArrowDownIcon />
         </span>
       ) : (
         children
       )}
       {rightIcon && (
         <span>
-          <ButtonArrowDown />
+          <ArrowDownIcon />
         </span>
       )}
     </button>
