c384752dfd86712026a4176c5cf4d4c9c9b0abf1
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2021-06-30 04:31:20 +0200

    Refacotr TextPollCreation Component

--- modules/shared/components/organisms/TextPollCreation/types/ITextPollCreation.d.ts
+++ modules/shared/components/organisms/TextPollCreation/types/ITextPollCreation.d.ts
@@ -1,28 +1,13 @@
-import type { PostCreationRequestTypeEnum } from '@m3ntorship/posts-client/dist/client';
-import type { IGetPosts } from '../../../../api/IGetPosts';
+import type { IPostCreation } from '../../PostCreation/types/IPostCreation';
 
 declare namespace ITextPollCreation {
   export interface IProps {
-    createTextPollPost: (state: IState) => Promise<IGetPosts.IErrorData>;
+    post: IPostCreation.IPostStructure;
+    postCreationGlobalState: IPostCreation.IState;
+    setPostCreationGlobalState: (state: IPostCreation.IState) => void;
   }
-  export interface IState {
-    postType: PostCreationRequestTypeEnum;
-    postCaption: { id: string; value: string };
-    groups: IGroups[];
-    hiddenIdentity: boolean;
-    privacy: string;
-    image?: string;
-  }
-
-  export interface IOption {
-    id: string;
-    body: string;
-  }
-
-  export interface IGroups {
-    id?: string;
-    name: string;
-    options: IOption[];
+  interface ITextPollMap {
+    textPoll: Record<string, Record<string, string>>;
   }
 }
 export { ITextPollCreation };
