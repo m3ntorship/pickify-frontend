5c94aea5bd056a16af27439921785c556ffcae6d
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2021-06-30 04:39:34 +0200

    Refactor UploadedFiles logic

--- modules/shared/hooks/useUpdatedImageData/useUpdatedImageData.ts
+++ modules/shared/hooks/useUpdatedImageData/useUpdatedImageData.ts
@@ -1,54 +1,17 @@
 import { useEffect, useState } from 'react';
-import { useIsMounted } from '../useIsMounted/useIsMounted';
 import type { IUseUpdatedImageData } from './IUseUpdatedImageData';
 
-export const useUpdatedImageData = (
-  props: IUseUpdatedImageData.UpdatedImageData,
-): string => {
-  const { file, imagesData, setImagesData, id, caption } = props;
+export const useUpdatedImageData = ({
+  file,
+}: IUseUpdatedImageData.UpdatedImageData): string => {
   const [url, setUrl] = useState<string>('');
-  const isMounted: React.MutableRefObject<boolean> = useIsMounted();
 
   useEffect(() => {
-    const { type } = file as File;
-    if (type) {
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(file as File);
-      fileReader.addEventListener('load', (e) => {
-        if (isMounted.current) {
-          setUrl(e.target?.result as string);
-        }
-      });
-    }
-  }, [file, isMounted]);
-
-  useEffect(() => {
-    if (url) {
-      const uploadedImages = imagesData.validImages.map((image) => {
-        if (image.imgId === id) {
-          return { ...image, file: url };
-        }
-        return image;
-      });
-      setImagesData({
-        ...imagesData,
-        validImages: uploadedImages,
-      });
-    }
-  }, [url]);
-
-  useEffect(() => {
-    const resetImagesCaption = imagesData.validImages.map((image) => {
-      if (image.imgId === id) {
-        return { ...image, imgCaption: caption };
-      }
-      return image;
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.addEventListener('load', (e: ProgressEvent<FileReader>) => {
+      setUrl(e.target?.result as string);
     });
-    setImagesData({
-      ...imagesData,
-      validImages: resetImagesCaption,
-    });
-  }, [caption]);
-
+  }, [file]);
   return url;
 };
--- modules/shared/logic/uploadedFiles/uploadedFiles.ts
+++ modules/shared/logic/uploadedFiles/uploadedFiles.ts
@@ -1,21 +1,18 @@
 import type { IUploadedFiles } from './IUploadedFiles';
 
 export const validateUploadedImages = (
-  files: File[] | FileList | null,
+  files: FileList | null,
 ): IUploadedFiles.IImagesData[] => {
   const firstIndex = 0;
   const lastLetter = 5;
 
-  const uploadedFiles = Array.prototype.map.call(files, (file: File) => {
-    if (file.type.substr(firstIndex, lastLetter) !== 'image') {
-      return {
-        error: true,
-        message: 'invalid file type, must be an image!!',
-      };
-    }
-
-    return { file, error: false };
-  }) as IUploadedFiles.IImagesData[];
-
-  return uploadedFiles;
+  const uploadedFiles = Array.prototype.map.call(files, (file: File) =>
+    file.type.substr(firstIndex, lastLetter) !== 'image'
+      ? {
+          error: true,
+          message: 'The uploaded file must be an image!',
+        }
+      : { error: false, file },
+  );
+  return uploadedFiles as IUploadedFiles.IImagesData[];
 };
