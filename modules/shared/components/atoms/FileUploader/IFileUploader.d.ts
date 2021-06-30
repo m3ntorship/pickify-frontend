e5084ddfa90f1d7fdc2f3cfa26d81581ff3f6952
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2021-06-30 05:04:49 +0200

    Fix FileUploader story

--- modules/shared/components/atoms/FileUploader/IFileUploader.d.ts
+++ modules/shared/components/atoms/FileUploader/IFileUploader.d.ts
@@ -1,9 +1,7 @@
 import type { IUploadedFiles } from '@modules/shared/logic/uploadedFiles/IUploadedFiles';
-import type { UseFormRegister } from 'react-hook-form';
 
 declare namespace IFileUploader {
   export interface IProps extends IHandlers {
-    register?: UseFormRegister<FieldValues>;
     maxFiles: number;
     lastFilesLength: number;
     entityType: 'group' | 'option' | 'post';
@@ -11,8 +9,6 @@ declare namespace IFileUploader {
 
   export interface IHandlers {
     onFileSuccess: (files: IUploadedFiles.IImagesData[]) => void;
-    // onFileError: (files: IUploadedFiles.IImagesData[]) => void;
-    // onMaxFilesError: (file: { error: boolean; message: string }) => void;
   }
 }
 export { IFileUploader };
