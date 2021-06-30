1014c6b25de1859e1199e8a3c64e07d6b2406aa3
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2021-06-30 04:24:55 +0200

    Refacotr FileUploader Component

--- modules/shared/components/atoms/FileUploader/IFileUploader.d.ts
+++ modules/shared/components/atoms/FileUploader/IFileUploader.d.ts
@@ -5,12 +5,14 @@ declare namespace IFileUploader {
   export interface IProps extends IHandlers {
     register?: UseFormRegister<FieldValues>;
     maxFiles: number;
+    lastFilesLength: number;
+    entityType: 'group' | 'option' | 'post';
   }
 
   export interface IHandlers {
     onFileSuccess: (files: IUploadedFiles.IImagesData[]) => void;
-    onFileError: (files: IUploadedFiles.IImagesData[]) => void;
-    onMaxFilesError: (file: { error: boolean; message: string }) => void;
+    // onFileError: (files: IUploadedFiles.IImagesData[]) => void;
+    // onMaxFilesError: (file: { error: boolean; message: string }) => void;
   }
 }
 export { IFileUploader };
