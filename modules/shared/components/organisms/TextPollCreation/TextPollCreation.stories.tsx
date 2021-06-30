610c79e1a8df1757709e6889f0eb6e25101f2f01
Ahmed Khitaby <ahmedkhitaby@gmail.com>
2021-06-30 06:23:20 +0200

    Fix TextPollCreation story

--- modules/shared/components/organisms/TextPollCreation/TextPollCreation.stories.tsx
+++ modules/shared/components/organisms/TextPollCreation/TextPollCreation.stories.tsx
@@ -1,15 +1,36 @@
+import { useState } from 'react';
 import type { ReactElement } from 'react';
 import type { Story } from '@storybook/react';
+import { useForm, FormProvider } from 'react-hook-form';
 import TextPollCreation from './TextPollCreation';
 import type { ITextPollCreation } from './types/ITextPollCreation';
+import postCreationInitialState from '../PostCreation/postCreationInitialState';
+import type { IPostCreation } from '../PostCreation/types/IPostCreation';
 
 export default {
   title: 'organisms/TextPollCreation',
   component: TextPollCreation,
 };
 
-const Template: Story<ITextPollCreation.IProps> = (args): ReactElement => (
-  <TextPollCreation {...args} />
-);
+const Template: Story<ITextPollCreation.IProps> = (args): ReactElement => {
+  const methods = useForm({
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+  });
+
+  const [postCreationGlobalState, setPostCreationGlobalState] =
+    useState<IPostCreation.IState>(postCreationInitialState);
+
+  return (
+    <FormProvider {...methods}>
+      <TextPollCreation
+        {...args}
+        post={postCreationGlobalState.textPoll}
+        setPostCreationGlobalState={setPostCreationGlobalState}
+        postCreationGlobalState={postCreationGlobalState}
+      />
+    </FormProvider>
+  );
+};
+
 export const Default = Template.bind({});
-Default.args = {};
