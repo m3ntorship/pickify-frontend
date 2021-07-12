17580b22a3397b5a6db3dec42c6024d40131a622
Ahmed Elsayad <ahmedelsayad98@gmail.com>
2021-07-12 18:41:04 +0200

    added regex to resolve the path for the tests file

--- jest.config.js
+++ jest.config.js
@@ -26,5 +26,6 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/mocks.js',
     '\\.(css|less)$': 'identity-obj-proxy',
+    '^@modules[/](.+)': '<rootDir>/modules/$1',
   },
 };
