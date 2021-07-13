bf823501bf2a056fab212fe3b3fb964bc4331e5b
omarawad0 <ogmal01@gmail.com>
2021-07-13 19:42:35 +0200

    added the tests for delete posts api

---
+++ modules/HomePage/api/DeletePostApi/IDeletePostsApi.d.ts
@@ -0,0 +1,19 @@
+declare namespace IDeletePostsApi {
+  export interface IDeletePostsRes {
+    resData: IDeletePostsErrorData | IDeletePostsSuccessData;
+  }
+  export interface IDeletePostsSuccessData {
+    message: string;
+    error: boolean;
+  }
+
+  export interface IDeletePostsErrorData {
+    message: string;
+    error: boolean;
+  }
+  export interface IDeletePostsResErrorData {
+    response?: { data: { message: string; status_code: number } };
+  }
+}
+
+export { IDeletePostsApi };
