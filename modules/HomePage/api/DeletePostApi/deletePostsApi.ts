09ce82f3b0e44b4a4cb10c228a20e8c5815386f1
ahmed <ahmed.twfiek8@gmail.com>
2021-07-17 17:42:45 +0200

    used generateErrMsg function for delete post error handling

--- modules/HomePage/api/DeletePostApi/deletePostsApi.ts
+++ modules/HomePage/api/DeletePostApi/deletePostsApi.ts
@@ -1,5 +1,7 @@
 import { postsApi } from '../../../shared/api/postsApi.api';
 import type { IDeletePostsApi } from './IDeletePostsApi';
+import { errorMessage } from './deletePostHandlers';
+import { generateErrMsg } from '../../../shared/logic/generateErrMsg/generateErrMsg';
 
 export const deletePost = async (
   postId: string,
@@ -8,34 +10,20 @@ export const deletePost = async (
     await postsApi.deleteOnePost(postId);
 
     return {
-      resData: {
-        error: false,
-        message: 'Post has been deleted successfully',
-      },
+      resData: { error: false, message: 'Post has been deleted successfully' },
     };
   } catch (error: unknown) {
     const { response } = error as IDeletePostsApi.IDeletePostsResErrorData;
-    let message = '';
-    if (!response) {
-      return {
-        resData: {
-          error: true,
-          message: 'Network Error',
-        },
-      };
-    }
-    switch (response.data.status_code) {
-      case 404:
-        message = 'Not Found';
-        break;
-      default:
-        message = 'Unauthorized';
-    }
-    return {
-      resData: {
-        error: true,
-        message,
-      },
-    };
+    const { message: errMessage } = error as { message: string };
+
+    if (!response) return { resData: { error: true, message: errMessage } };
+
+    const { data } = response;
+
+    const { message, status_code } = data;
+
+    const generatedMessage = generateErrMsg(errorMessage, status_code, message);
+
+    return { resData: { error: true, message: generatedMessage } };
   }
 };
