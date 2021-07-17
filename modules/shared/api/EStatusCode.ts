b6c93892c7157b4120874d3fb049600449a0ad96
ahmed <ahmed.twfiek8@gmail.com>
2021-07-17 17:36:34 +0200

    created status code enum for custom error msg

---
+++ modules/shared/api/EStatusCode.ts
@@ -0,0 +1,11 @@
+export enum EStatusCode {
+  Ok = 200,
+  Created = 201,
+  Accepted = 202,
+  BadRequest = 400,
+  Unauthorized = 401,
+  NotFound = 404,
+  Conflict = 409,
+  ManyRequests = 429,
+  InternalServerError = 500,
+}
