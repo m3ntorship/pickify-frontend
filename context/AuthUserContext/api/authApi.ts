83bd90d65c7a3c3a9089857e1ee6aee79ea3527c
ahmed <ahmed.twfiek8@gmail.com>
2021-07-28 10:07:58 +0200

    moved auth api logic to the auth context folder

---
+++ context/AuthUserContext/api/authApi.ts
@@ -0,0 +1,62 @@
+import { firebaseAuth } from '@modules/shared/api/auth';
+import { generateErrMsg } from '@modules/shared/logic/generateErrMsg/generateErrMsg';
+import {
+  setUserToken,
+  setUserUUID,
+} from '@modules/shared/logic/userAuth/userAuth';
+import axios from 'axios';
+import firebase from 'firebase';
+import type { IAuth } from './IAuth';
+
+export const logoutUser = async (): Promise<void> => {
+  await firebaseAuth.signOut();
+};
+
+export const loginUser = async (): Promise<string | undefined> => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const data = await firebaseAuth.signInWithPopup(provider);
+  const token = await data.user?.getIdToken();
+  setUserToken(token ?? '');
+  return token;
+};
+
+export const register = async (
+  token: string | undefined,
+): Promise<IAuth.IAuthResData> => {
+  try {
+    const { data }: { data: { uuid: string } } = await axios.post(
+      'https://pickify-posts-be-dev.m3ntorship.net/api/users/register',
+      undefined,
+      {
+        headers: {
+          Authorization: `Bearer ${token ?? ''}`,
+        },
+      },
+    );
+
+    const { uuid } = data;
+    setUserUUID(uuid);
+
+    return {
+      resData: { error: false, message: 'You have logged in successfully!' },
+    };
+  } catch (error: unknown) {
+    const { response } = error as IAuth.IErrorData;
+    const { message: errMessage } = error as { message: string };
+
+    if (!response) return { resData: { error: true, message: errMessage } };
+
+    const { data } = response;
+
+    const { message, status_code } = data;
+
+    const generatedMessage = generateErrMsg({}, status_code, message);
+    return {
+      resData: {
+        error: true,
+        message: generatedMessage,
+        errorCode: status_code,
+      },
+    };
+  }
+};
