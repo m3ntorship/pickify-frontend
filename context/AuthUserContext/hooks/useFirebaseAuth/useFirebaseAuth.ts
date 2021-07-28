365679291f02af47857ef90d6dbecf1af751fd26
ahmed <ahmed.twfiek8@gmail.com>
2021-07-28 10:09:35 +0200

    created custom hook to listen to user state changes

---
+++ context/AuthUserContext/hooks/useFirebaseAuth/useFirebaseAuth.ts
@@ -0,0 +1,67 @@
+import type firebase from 'firebase';
+import {
+  clearUserToken,
+  clearUserUUID,
+  getUserToken,
+  setUserToken,
+} from '@modules/shared/logic/userAuth/userAuth';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { firebaseAuth } from '../../../../modules/shared/api/auth';
+import type { IUseFirebaseAuth } from './IUseFirebaseAuth';
+
+const setUserData = (
+  user: firebase.User | null,
+): IUseFirebaseAuth.IUserData => ({
+  username: user?.displayName ?? '',
+  email: user?.email ?? '',
+  userImg: user?.photoURL ?? '',
+});
+
+export const useFirebaseAuth = (): IUseFirebaseAuth.IProps => {
+  const [user, setUser] = useState<IUseFirebaseAuth.IUserData | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const clearUserState = (): void => {
+    setUser(null);
+    setLoading(false);
+    clearUserUUID();
+    clearUserToken();
+    setIsAuthenticated(false);
+  };
+
+  const authStateChanged = async (
+    userState: firebase.User | null,
+  ): Promise<void> => {
+    if (!userState) {
+      clearUserState();
+      return;
+    }
+    setLoading(true);
+    try {
+      const token: string = await userState.getIdToken();
+      const loggedInUser = getUserToken();
+      const formattedUser = setUserData(userState);
+      setUser(formattedUser);
+      setLoading(false);
+      setIsAuthenticated(true);
+      if (loggedInUser) {
+        setUserToken(token);
+      }
+    } catch (error: unknown) {
+      const { message } = error as { message: string };
+      toast.error(message);
+      clearUserState();
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = firebaseAuth.onAuthStateChanged(authStateChanged);
+    return (): void => {
+      unsubscribe();
+    };
+  }, []);
+
+  return { user, loading, isAuthenticated };
+};
