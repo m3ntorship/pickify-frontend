8d4000c1a310d983f3fef21dea46ce3ac3ef2eec
ahmed <ahmed.twfiek8@gmail.com>
2021-07-28 11:11:31 +0200

    import only 'firebase/app' SDK instead of 'firebase' all comps

--- context/AuthUserContext/api/authApi.ts
+++ context/AuthUserContext/api/authApi.ts
@@ -5,7 +5,7 @@ import {
   setUserUUID,
 } from '@modules/shared/logic/userAuth/userAuth';
 import axios from 'axios';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import type { IAuth } from './IAuth';
 
 export const logoutUser = async (): Promise<void> => {
