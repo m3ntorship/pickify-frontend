e6da5658bc6473cae45dabb28303b63e5c89b186
ahmed <ahmed.twfiek8@gmail.com>
2021-08-02 16:33:12 +0200

    used built in context provider from next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,27 @@ import wrapper from 'store';
 import type { AppProps } from 'next/app';
 import type { ReactElement } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import { AuthUserProvider } from '../context/AuthUserContext/AuthUserContext';
+import { Provider } from 'next-auth/client';
+import type { Session } from 'next-auth';
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 
 toast.configure();
 
-const Pickly = ({ Component, pageProps }: AppProps): ReactElement => (
-  <AuthUserProvider>
-    <ToastContainer limit={2} />
-    <Component {...pageProps} />
-  </AuthUserProvider>
-);
+const Pickly = ({ Component, pageProps }: AppProps): ReactElement => {
+  const { session } = pageProps as { session: Session };
+  return (
+    <Provider
+      session={session}
+      options={{
+        clientMaxAge: 60, // Re-fetch session if cache is older than 60 seconds
+        keepAlive: 5 * 60, // Send keepAlive message every 5 minutes
+      }}
+    >
+      <ToastContainer limit={2} />
+      <Component {...pageProps} />
+    </Provider>
+  );
+};
 
 export default wrapper.withRedux(Pickly);
