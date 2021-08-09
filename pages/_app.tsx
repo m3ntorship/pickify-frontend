09a5b1349fc3912390155ab77376947c96155d62
Ahmed Elsayad <ahmedelsayad98@gmail.com>
2021-08-09 14:29:55 +0200

    used react-scroll-to-top package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,13 @@ import '../styles/globals.css';
 import React from 'react';
 import Navigation from '@modules/shared/components/molecules/Navigation/Navigation';
 import { useRouter } from 'next/router';
+import ScrollToTop from 'react-scroll-to-top';
+import Button from '@modules/shared/components/atoms/Button/Button';
 import styles from './_app.module.css';
 import { AuthUserProvider } from '../context/AuthUserContext/AuthUserContext';
+import * as EButton from '../modules/shared/components/atoms/Button/types/EButton';
 
+// import ArrowDown from "../modules/shared/components/icons/arrowDown.svg";
 toast.configure();
 
 const Pickly = ({ Component, pageProps }: AppProps): ReactElement => {
@@ -18,6 +22,18 @@ const Pickly = ({ Component, pageProps }: AppProps): ReactElement => {
 
   return (
     <AuthUserProvider>
+      <ScrollToTop
+        smooth
+        component={
+          <div className="transform rotate-180">
+            <Button
+              onlyIcon
+              variant={EButton.buttonVariantValues.PRIMARY}
+              size={EButton.buttonSizeValues.MEDIUM}
+            />
+          </div>
+        }
+      />
       <div className={styles.navigation}>{showHeader && <Navigation />}</div>
       <ToastContainer limit={2} />
       <div className="px-4 md:px-6">
