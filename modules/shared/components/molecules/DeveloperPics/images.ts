707939f45345ceeb048acf1ad25ecdd30d2bea8d
AhmedAdel999 <ahmedadel555441@gmail.com>
2021-08-10 18:58:14 +0200

    Added DeveloperPics component

---
+++ modules/shared/components/molecules/DeveloperPics/images.ts
@@ -0,0 +1,14 @@
+export const images: string[] = [
+  '/Ayoub.jpeg',
+  '/Elhaw.jpeg',
+  '/Adel.jpg',
+  '/Twfiek.jpeg',
+  '/Omar.jpg',
+  '/Sayad.jpg',
+  '/Essam.jpeg',
+  '/Sahl.jpeg',
+  '/Samy.jpeg',
+  '/Shaf3y.jpeg',
+  '/Ali.jpeg',
+  '/Amr.jpeg',
+];
--- modules/shared/components/organisms/Widgets/Widgets.tsx
+++ modules/shared/components/organisms/Widgets/Widgets.tsx
@@ -1,5 +1,6 @@
 import type { FC, ReactElement } from 'react';
 import React from 'react';
+import DeveloperPics from '../../molecules/DeveloperPics/DeveloperPics';
 import FriendSuggestions from '../FriendSuggestions/FriendSuggestions';
 import TrendingQuestions from '../TrendingQuestions/TrendingQuestions';
 
@@ -7,6 +8,9 @@ const Widgets: FC = (): ReactElement => {
   return (
     <div className="w-21xl">
       <div className="rounded-md p-6 bg-white hidden md:block">
+        <DeveloperPics />
+      </div>
+      <div className="mt-6 rounded-md p-6 bg-white hidden md:block">
         <TrendingQuestions />
       </div>
       <div className="p-6 mt-6 bg-white hidden md:block">
