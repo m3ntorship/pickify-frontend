249e0c2d200dd0ee56d208bf2113510c503ab08d
ahmed <ahmed.twfiek8@gmail.com>
2021-08-11 12:15:35 +0200

    improve getNumberOfVisibleElements tests and covered all the test cases

--- modules/shared/components/molecules/Footer/footerLogic/footerLogic.test.ts
+++ modules/shared/components/molecules/Footer/footerLogic/footerLogic.test.ts
@@ -46,14 +46,58 @@ describe('getTotalItemsWidth', () => {
 });
 
 describe('getNumberOfVisibleElements', () => {
-  it('should return the total width of each link + total margin', () => {
+  it('should return 2 elements when the list parent width equals the width of the fist 2 links + their margin + the button width', () => {
     const elements = [
       { offsetWidth: 12 },
       { offsetWidth: 20 },
       { offsetWidth: 25 },
     ];
 
-    const listWidth = 50;
+    const listWidth = 76;
+
+    const buttonWdith = 20;
+
+    const visibleElements = getNumberOfVisibleElements(
+      elements,
+      listWidth,
+      buttonWdith,
+    );
+
+    const expectedRes = 2;
+
+    expect(visibleElements).toEqual(expectedRes);
+  });
+
+  it('should return 2 elements when the list parent width is greater than the width of the fist 2 links but less than 3 links', () => {
+    const elements = [
+      { offsetWidth: 12 },
+      { offsetWidth: 20 },
+      { offsetWidth: 25 },
+    ];
+
+    const listWidth = 85;
+
+    const buttonWdith = 20;
+
+    const visibleElements = getNumberOfVisibleElements(
+      elements,
+      listWidth,
+      buttonWdith,
+    );
+
+    const expectedRes = 2;
+
+    expect(visibleElements).toEqual(expectedRes);
+  });
+
+  it('should return 1 element when the difference between the the list parent width and the first 2 links is only 1 px to ensure that the design is not going to break in this case', () => {
+    const elements = [
+      { offsetWidth: 12 },
+      { offsetWidth: 20 },
+      { offsetWidth: 25 },
+    ];
+
+    const listWidth = 75;
 
     const buttonWdith = 20;
 
