64c3802239f9c278c24820a8fce34c8d77eb9987
omarawad0 <ogmal01@gmail.com>
2021-08-12 17:58:17 +0200

    improved styling and responsivness

--- modules/HomePage/pages/FriendsPage/FriendsPage.tsx
+++ modules/HomePage/pages/FriendsPage/FriendsPage.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import type { FC, ReactElement } from 'react';
 import FriendLoader from '@modules/shared/components/atoms/FriendLoader/FriendLoader';
+import styles from './FriendsPage.module.css';
 
 const FriendsPage: FC = (): ReactElement => {
   const numberOfNotification = Array.from(Array(12).keys());
-
   return (
-    <div className="">
-      <h1 className="text-lg text-dark mb-l">Friends</h1>
-      <h1 className="text-3xl font-bold text-dark-grey whitespace-nowrap z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        Coming soon
-      </h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
+    <div>
+      <h1 className={styles.header}>Friends</h1>
+      <div className={styles['loaders-wrapper']}>
         {numberOfNotification.map((n) => {
           return (
-            <div className="bg-white shadow-soft p-4 rounded-md mb-xs w-max ">
-              <FriendLoader key={n} />
+            <div key={n} className={styles.loader}>
+              <FriendLoader />
             </div>
           );
         })}
+        <h1 className={styles['soon-text']}>Coming soon</h1>
       </div>
     </div>
   );
