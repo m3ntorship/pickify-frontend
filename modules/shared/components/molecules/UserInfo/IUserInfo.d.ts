8c8d00c9b576853633f6bf9a872c57cbc0b7e063
ahmed <ahmed.twfiek8@gmail.com>
2021-08-14 17:40:18 +0200

    refactor UserInfo component and updated PostViewHeader

--- modules/shared/components/molecules/UserInfo/IUserInfo.d.ts
+++ modules/shared/components/molecules/UserInfo/IUserInfo.d.ts
@@ -1,11 +1,10 @@
-import type { IAvatar } from '../../atoms/Avatar/IAvatar';
-
 declare namespace IUserInfo {
-  export interface IProps extends IAvatar.IData, IData {}
-  export interface IData extends IAvatar.IData {
+  export interface IProps {
+    profilePic?: string;
     isHidden: boolean;
-    name?: string;
-    date: Date;
+    title?: string;
+    subTitle: string;
+    description?: string;
   }
 }
 
