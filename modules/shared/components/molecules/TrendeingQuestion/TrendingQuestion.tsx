b2cfc360c8d029e84bae897f8739459435c31301
ahmed <ahmed.twfiek8@gmail.com>
2021-08-16 13:00:25 +0200

    used UserInfo in TrendingQuestion & removed truncate function

--- modules/shared/components/molecules/TrendeingQuestion/TrendingQuestion.tsx
+++ modules/shared/components/molecules/TrendeingQuestion/TrendingQuestion.tsx
@@ -1,37 +1,29 @@
 import React from 'react';
 import type { FC, ReactElement } from 'react';
-import Link from 'next/link';
 import ImagePoll from '../../icons/imagePoll.svg';
 import TextPoll from '../../icons/textPoll.svg';
 import MiniSurvey from '../../icons/miniSurvey.svg';
 import type { ITrendingQuestion } from './ITrendingQuestion';
 import styles from './TrendingQuestion.module.css';
-
-export const truncate = (str: string): string => {
-  const limit = 30;
-  return str.length > limit ? `${str.substr(0, limit - 1)}...` : str;
-};
+import UserInfo from '../UserInfo/UserInfo';
 
 const TrendingQuestion: FC<ITrendingQuestion.IProps> = ({
   postCaption,
   type,
 }): ReactElement => {
   return (
-    <div className="flex">
+    <UserInfo
+      isHidden={false}
+      variant="icon"
+      title={postCaption}
+      subTitle={type}
+    >
       <div className={styles['trending-question']}>
         {type === 'Mini Survey' && <MiniSurvey />}
         {type === 'Text Poll' && <TextPoll />}
         {type === 'Image Poll' && <ImagePoll />}
       </div>
-      <div className="w-60">
-        <div className="flex">
-          <Link href="/">
-            <a>{truncate(postCaption)}</a>
-          </Link>
-        </div>
-        <p className="text-xs text-dark-grey font-light">{type}</p>
-      </div>
-    </div>
+    </UserInfo>
   );
 };
 
