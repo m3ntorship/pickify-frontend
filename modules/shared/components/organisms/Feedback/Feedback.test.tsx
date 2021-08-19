a77b531909ae58b12c08a0272810f5b9d2c31116
omarawad0 <ogmal01@gmail.com>
2021-08-19 20:32:08 +0200

    added tests to the feedback

--- modules/shared/components/organisms/Feedback/Feedback.test.tsx
+++ modules/shared/components/organisms/Feedback/Feedback.test.tsx
@@ -10,44 +10,98 @@ describe('Feedback', () => {
     expect(tree).toMatchSnapshot();
   });
 
+  it('clicking on the happyIcon in the navigation should open the feedback popup', () => {
+    render(<Feedback />);
+    const openFeedback = screen.getByTestId('open-feedback');
+    userEvent.click(openFeedback);
+    const feedBackPopup = screen.getByTestId('feedback-popup');
+
+    expect(feedBackPopup).toBeInTheDocument();
+  });
+
   it('renders The Feedback with disabled button as default', () => {
     render(<Feedback />);
-
+    const openFeedback = screen.getByTestId('open-feedback');
+    userEvent.click(openFeedback);
     const submitButton = screen.getByRole('button');
 
     expect(submitButton).toBeDisabled();
   });
 
-  it('renders The Feedback with disabled button as default and when user chose any emoji it will be activated', () => {
+  it('renders The Feedback with disabled button as default and when user choose any emoji it will be activated', () => {
     render(<Feedback />);
-    const goodEmoji = screen.getByTestId('good');
+    const openFeedback = screen.getByTestId('open-feedback');
+    userEvent.click(openFeedback);
+    const goodEmoji = screen.getByTestId('4');
     const submitButton = screen.getByRole('button');
-
     userEvent.click(goodEmoji);
 
     expect(submitButton).toBeEnabled();
   });
 
-  it('renders The Feedback when user click happy or amazing emoji state', () => {
+  it('renders The positive Feedback when user click Amazing emoji with 5 rate', () => {
     render(<Feedback />);
-
-    const goodEmoji = screen.getByTestId('good');
+    const openFeedback = screen.getByTestId('open-feedback');
+    userEvent.click(openFeedback);
+    const amazingEmoji = screen.getByTestId('5');
     const submitButton = screen.getByRole('button');
-
-    userEvent.click(goodEmoji);
+    userEvent.click(amazingEmoji);
     userEvent.click(submitButton);
 
     const positiveFeedback = screen.getByTestId('positiveFeedback');
 
     expect(positiveFeedback).toBeInTheDocument();
   });
-  it('renders The Feedback when user click bad or neutral or terrible emoji state', () => {
+
+  it('renders The positive Feedback when user click Happy emoji with 4 rate', () => {
     render(<Feedback />);
-
-    const badEmoji = screen.getByTestId('bad');
+    const openFeedback = screen.getByTestId('open-feedback');
+    userEvent.click(openFeedback);
+    const happyEmoji = screen.getByTestId('4');
     const submitButton = screen.getByRole('button');
+    userEvent.click(happyEmoji);
+    userEvent.click(submitButton);
 
-    userEvent.click(badEmoji);
+    const positiveFeedback = screen.getByTestId('positiveFeedback');
+
+    expect(positiveFeedback).toBeInTheDocument();
+  });
+
+  it('renders The negative Feedback when user click neutral emoji state with 3 rate', () => {
+    render(<Feedback />);
+    const openFeedback = screen.getByTestId('open-feedback');
+    userEvent.click(openFeedback);
+    const neutralEmoji = screen.getByTestId('3');
+    const submitButton = screen.getByRole('button');
+    userEvent.click(neutralEmoji);
+    userEvent.click(submitButton);
+
+    const negativeFeedback = screen.getByTestId('negativeFeedback');
+
+    expect(negativeFeedback).toBeInTheDocument();
+  });
+
+  it('renders The negative Feedback when user click sad emoji state with 2 rate', () => {
+    render(<Feedback />);
+    const openFeedback = screen.getByTestId('open-feedback');
+    userEvent.click(openFeedback);
+    const sadEmoji = screen.getByTestId('2');
+    const submitButton = screen.getByRole('button');
+    userEvent.click(sadEmoji);
+    userEvent.click(submitButton);
+
+    const negativeFeedback = screen.getByTestId('negativeFeedback');
+
+    expect(negativeFeedback).toBeInTheDocument();
+  });
+
+  it('renders The negative Feedback when user click very sad emoji state with 1 rate', () => {
+    render(<Feedback />);
+    const openFeedback = screen.getByTestId('open-feedback');
+    userEvent.click(openFeedback);
+    const verySadEmoji = screen.getByTestId('1');
+    const submitButton = screen.getByRole('button');
+    userEvent.click(verySadEmoji);
     userEvent.click(submitButton);
 
     const negativeFeedback = screen.getByTestId('negativeFeedback');
