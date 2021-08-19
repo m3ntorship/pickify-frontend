aaaca81ec205f442f2d575b61affe6b737483f80
AhmedAdel999 <ahmedadel555441@gmail.com>
2021-08-20 00:07:34 +0200

    Set user id on auth state changed

--- modules/Login/pages/Login.tsx
+++ modules/Login/pages/Login.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-import type { FC, ReactElement, ReactText } from 'react';
+import React, { useEffect } from 'react';
+import type { FC, ReactElement } from 'react';
 
-import { toast } from 'react-toastify';
-import Head from 'next/head';
-import {
-  loginUser,
-  register,
-} from '../../../context/AuthUserContext/api/authApi';
+import { loginUser } from '../../../context/AuthUserContext/api/authApi';
 import { useRedirect } from '../../shared/hooks/useRedirect/useRedirect';
 import { useAuth } from '../../../context/AuthUserContext/AuthUserContext';
 import {
@@ -22,22 +17,9 @@ import Box from '../../shared/components/atoms/Box/Box';
 const Login: FC = (): ReactElement => {
   const { loading, isAuthenticated } = useAuth();
   const { redirectToHomePage, redirectToPostPage } = useRedirect();
-  const toastId = useRef<ReactText>();
 
   useEffect(() => {
     if (!loading && isAuthenticated) {
-      redirectToHomePage();
-    }
-  }, [isAuthenticated, loading]);
-
-  const login = async (): Promise<void> => {
-    const token: string | undefined = await loginUser();
-    toastId.current = toast.warning('Please wait while logging', {
-      autoClose: false,
-    });
-    const { resData } = await register(token);
-    toast.dismiss(toastId.current);
-    if (!resData.error) {
       const lastPage = getLastPage();
       if (lastPage) {
         redirectToPostPage(lastPage);
@@ -45,55 +27,51 @@ const Login: FC = (): ReactElement => {
       } else {
         redirectToHomePage();
       }
-      toast.success(resData.message);
-    } else {
-      toast.error(resData.message);
     }
+  }, [isAuthenticated, loading]);
+
+  const login = async (): Promise<void> => {
+    await loginUser();
   };
   return (
-    <>
-      <Head>
-        <title>Login | Pickify</title>
-      </Head>
-      <div className={styles['login-body-wrapper']}>
-        <div className={styles['login-body']}>
-          <div className={styles['main-wrapper']}>
-            <header>
-              <Logo />
-            </header>
-            <Box isWhiteColor>
-              <Box.Body classes={styles.main}>
-                <>
-                  <h1 className={styles['main-h1']}>Welcome to Pickify</h1>
-                  <p className={styles['main-paragraph']}>
-                    Pickify is an online platform that helps people make better
-                    decisions through voting insights
-                  </p>
-                  <button
-                    type="button"
-                    data-testid="login-test-button"
-                    onClick={login}
-                    className={styles['sign-in-button']}
-                  >
-                    <div className={styles['google-logo']}>
-                      <div>
-                        <Google />
-                      </div>
+    <div className={styles['login-body-wrapper']}>
+      <div className={styles['login-body']}>
+        <div className={styles['main-wrapper']}>
+          <header>
+            <Logo />
+          </header>
+          <Box isWhiteColor>
+            <Box.Body classes={styles.main}>
+              <>
+                <h1 className={styles['main-h1']}>Welcome to Pickify</h1>
+                <p className={styles['main-paragraph']}>
+                  Pickify is an online platform that helps people make better
+                  decisions through voting insights
+                </p>
+                <button
+                  type="button"
+                  data-testid="login-test-button"
+                  onClick={login}
+                  className={styles['sign-in-button']}
+                >
+                  <div className={styles['google-logo']}>
+                    <div>
+                      <Google />
                     </div>
-                    <span className={styles['sign-in-button-paragraph']}>
-                      Sign in with Google
-                    </span>
-                  </button>
-                </>
-              </Box.Body>
-            </Box>
-          </div>
-          <div>
-            <Footer />
-          </div>
+                  </div>
+                  <span className={styles['sign-in-button-paragraph']}>
+                    Sign in with Google
+                  </span>
+                </button>
+              </>
+            </Box.Body>
+          </Box>
+        </div>
+        <div>
+          <Footer />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
