a95fa2c8e62c2da86aeafde496ebbbe5d7b1c559
Ahmed Elsayad <ahmedelsayad98@gmail.com>
2021-08-21 14:55:24 +0200

    redirect the user to the same page after login

--- pages/friends/index.tsx
+++ pages/friends/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import type { FC, ReactElement } from 'react';
 import FriendsPage from '@modules/FriendsPage/FriendsPage';
 import type { GetServerSideProps } from 'next';
+import type { CookieSerializeOptions } from 'cookie';
+import { serialize } from 'cookie';
 
 const index: FC = (): ReactElement => {
   return <FriendsPage />;
@@ -15,6 +17,17 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const data = await Promise.resolve(true);
 
   if (!user) {
+    const { url } = context.req as { url: string };
+    const serializeOptions: CookieSerializeOptions = {
+      httpOnly: false,
+      secure: true,
+      maxAge: 60 * 60 * 24 * 31,
+      path: '/',
+    };
+    context.res.setHeader(
+      'Set-Cookie',
+      serialize('lastPage', url, serializeOptions),
+    );
     return {
       redirect: {
         destination: '/login',
