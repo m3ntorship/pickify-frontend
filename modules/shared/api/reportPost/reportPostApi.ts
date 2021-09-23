21eda1631fa050f7dd538ad7145b999421e5b21f
ahmed <ahmed.twfiek8@gmail.com>
2021-09-23 11:55:31 +0200

    created reportPostApi with it's return type for reports endpoint

---
+++ modules/shared/api/reportPost/reportPostApi.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import type { IReportPostApi } from './IReportPostApi';
+import { errorMessage } from './reportPostHelpers';
+import { generateErrMsg } from '../../logic/generateErrMsg/generateErrMsg';
+
+export const reportPost = async (
+  postId: string,
+  token: string,
+): Promise<IReportPostApi.IReportPostRes> => {
+  try {
+    await axios.post(
+      `https://pickify-posts-be-dev.m3ntorship.net/api/reports`,
+      { report_type: 'post', post_id: postId },
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+
+    return { resData: { message: 'Post has been reported successfully' } };
+  } catch (err: unknown) {
+    const { response } = err as IReportPostApi.IReportPostResErrorData;
+    const { message: errMessage } = err as { message: string };
+
+    if (!response) {
+      throw Object.assign(new Error(), {
+        resData: { error: true, message: errMessage },
+      });
+    }
+
+    const { message, status_code } = response.data;
+    const generatedMessage = generateErrMsg(errorMessage, status_code, message);
+
+    throw Object.assign(new Error(), {
+      resData: {
+        error: true,
+        message: generatedMessage,
+        errorCode: status_code,
+      },
+    });
+  }
+};
